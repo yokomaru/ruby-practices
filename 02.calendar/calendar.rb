7c1480991ecc38fe9819ff9c9b986e3566512e20
yokomaru <cabochetoganlan@gmail.com>
2024-01-30 23:28:00 +0900

    add: 当日の場合背景と文字が入れ替わる処理を追加

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -12,7 +12,9 @@ first_date = Date.new(year, month, 1)
 last_date = Date.new(year, month, -1)
 
 calender_array = (first_date..last_date).each_with_object([]) do |date, a|
-  a << if date.wday == 0
+  a << if date == Date.today
+        date.day.to_s.length == 1 ? " \e[30m\e[47m#{date.day}\e[0m" : "\e[30m\e[47m#{date.day}\e[0m"
+      elsif date.wday == 0
         date.day.to_s.length == 1 ? " \e[31m#{date.day}\e[0m" : "\e[31m#{date.day}\e[0m"
       elsif date.wday == 6
         date.day.to_s.length == 1 ? " \e[34m#{date.day}\e[0m" : "\e[34m#{date.day}\e[0m"
