4a30e29e51f820ed408a5b5d2d028d11382e8d60
yokomaru <cabochetoganlan@gmail.com>
2024-01-30 23:49:42 +0900

    fix: def で囲ってメソッドに修正＋calコマンドの見た目に近づけるために最後に空の行を一列追加

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -4,33 +4,36 @@
 require 'optparse'
 require 'date'
 
-params = ARGV.getopts('m:', 'y:')
+def calender
+  params = ARGV.getopts('m:', 'y:')
+  month = params['m'].nil? || !params['m'].match?(/\A[0-9]+\z/) || !params['m'].to_i.between?(1, 12) ? Date.today.month.to_i : params['m'].to_i
+  year = params['y'].nil? || !params['y'].match?(/\A[0-9]+\z/) || !params['y'].to_i.between?(1970, 2100) ? Date.today.year.to_i : params['y'].to_i
+  first_date = Date.new(year, month, 1)
+  last_date = Date.new(year, month, -1)
 
-month = params['m'].nil? || !params['m'].match?(/\A[0-9]+\z/) || !params['m'].to_i.between?(1, 12) ? Date.today.month.to_i : params['m'].to_i
-year = params['y'].nil? || !params['y'].match?(/\A[0-9]+\z/) || !params['y'].to_i.between?(1970, 2100) ? Date.today.year.to_i : params['y'].to_i
+  calender_days = (first_date..last_date).each_with_object([]) do |date, a|
+    a << if date == Date.today
+           date.day.to_s.length == 1 ? " \e[30m\e[47m#{date.day}\e[0m" : "\e[30m\e[47m#{date.day}\e[0m"
+         else
+           date.day.to_s.length == 1 ? " #{date.day}" : date.day.to_s
+         end
+  end
 
-first_date = Date.new(year, month, 1)
-last_date = Date.new(year, month, -1)
+  first_date.wday.times do
+    calender_days.unshift('　')
+  end
 
-calender_days = (first_date..last_date).each_with_object([]) do |date, a|
-  a << if date == Date.today
-         date.day.to_s.length == 1 ? " \e[30m\e[47m#{date.day}\e[0m" : "\e[30m\e[47m#{date.day}\e[0m"
-       else
-         date.day.to_s.length == 1 ? " #{date.day}" : date.day.to_s
-       end
+  carendar_display_datas = []
+  carendar_display_datas << ['　', '　', "#{first_date.month}月", first_date.year, '　', '　', '　']
+  carendar_display_datas << %w[日 月 火 水 木 金 土]
+  calender_days.each_slice(7) do |calender_day|
+    carendar_display_datas << calender_day
+  end
+  carendar_display_datas << []
+
+  carendar_display_datas.each do |carendar_display_data|
+    puts carendar_display_data.join(' ')
+  end
 end
 
-first_date.wday.times do
-  calender_days.unshift('　')
-end
-
-carendar_display_datas = []
-carendar_display_datas << ['　', '　', "#{first_date.month}月", first_date.year, '　', '　', '　']
-carendar_display_datas << %w[日 月 火 水 木 金 土]
-calender_days.each_slice(7) do |calender_day|
-  carendar_display_datas << calender_day
-end
-
-carendar_display_datas.each do |carendar_display_data|
-  puts carendar_display_data.join(' ')
-end
+calender
