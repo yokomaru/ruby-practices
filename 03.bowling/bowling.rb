f69ec80d14fa5fa537b024ab7af9950cc611738b
yokomaru <cabochetoganlan@gmail.com>
2024-02-01 19:41:43 +0900

    add: 20個のカンマ区切りの引数を受け取り、配列に変換した後合計値を計算して表示する処理を作成

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -3,7 +3,15 @@
 require 'optparse'
 
 def bowling
+  # 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
   p ARGV[0]
+  score =  ARGV[0].split(",").map(&:to_i)
+  shot = []
+  10.times do
+    shot << score.pop(2)
+  end
+  puts shot.sum(&:sum)
 end
 
 bowling
+
