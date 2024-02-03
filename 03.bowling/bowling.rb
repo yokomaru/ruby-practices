aaa2c1ce6fb2a534dbbb666300dd8d38f16111b1
yokomaru <cabochetoganlan@gmail.com>
2024-02-03 22:40:43 +0900

    fix: each_with_objectをmapにして１行に修正+不要なindexを削除

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
+
 STRIKE_SCORE = 10
 STRIKE_NEXT_FRAME = 1
 SPARE_SCORE = 10
@@ -9,17 +10,13 @@ FRAME_TIMES = 10
 
 def bowling
   scores = ARGV[0].split(',')
-  shots = scores.each_with_object([]) do |score, shot|
-    if score == 'X'
-      shot << STRIKE_SCORE
-    else
-      shot << score.to_i
-    end
-  end
+  shots = []
+  shots = scores.map { |score| score == 'X' ? STRIKE_SCORE : score.to_i }
 
   total_points = 0
   frame = 0
-  FRAME_TIMES.times do |index|
+
+  FRAME_TIMES.times do
     if shots[frame] == STRIKE_SCORE
       total_points += shots[frame] + shots[frame + 1] + shots[frame + 2]
       frame += STRIKE_NEXT_FRAME
