35f97d8cda1ed292f5e942473360ce1c6ee66ef9
yokomaru <cabochetoganlan@gmail.com>
2024-02-03 18:09:08 +0900

    fix: 不要なコメント削除

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -24,25 +24,17 @@ def bowling
 
   point = 0
   frames.each_with_index do |frame, index|
-    # 10フレーム目以降は計算しない
     break if index > LAST_FRAME
 
-    # ストライクだった場合
     point += if frame[0] == STRIKE_FIRST_SHOT_SCORE
-                # 次もストライクだった場合
                 if frames[index + 1][0] == STRIKE_FIRST_SHOT_SCORE
-                  # 次とその次のフレームの１投目を合計した値を合算する
                   frame[0] + frames[index + 1][0] + frames[index + 2][0]
-                  # 次がストライクじゃなかった場合は次のフレームの１投目と２投目を合算する
                 else
                   frame[0] + frames[index + 1][0] + frames[index + 1][1]
                 end
-              # スペアだった場合
             elsif frame.sum == SPARE_SUM_SCORE
-              # 次のフレームの１投目を合算する
               frame.sum + frames[index + 1][0]
             else
-              # 現在のフレームの合計を合算する
               frame.sum
             end
   end
