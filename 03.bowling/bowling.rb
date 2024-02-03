0cf1f0fbfd39a85fd0bf983e11b1185cd6594825
yokomaru <cabochetoganlan@gmail.com>
2024-02-03 16:58:05 +0900

    add: ボウリングの得点計算処理を追加

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -17,7 +17,30 @@ def bowling
     frames << shot
   end
 
-  puts frames
+  point = 0
+  frames.each_with_index do |frame, index|
+    # 10フレーム目以降は計算しない
+    break if index > 9
+    # ストライクだった場合
+    if frame[0] == 10
+      # 次もストライクだった場合
+      if frames[index + 1][0] == 10
+        # 次とその次のフレームの１投目を合計した値を合算する
+        point += frame[0] + frames[index + 1][0] + frames[index + 2][0]
+      # 次がストライクじゃなかった場合は次のフレームの１投目と２投目を合算する
+      else
+        point += frame[0] + frames[index + 1][0] + frames[index + 1][1]
+      end
+    # スペアだった場合
+    elsif frame.sum == 10
+      # 次のフレームの１投目を合算する
+      point += frame.sum + frames[index + 1][0]
+    else
+      # 現在のフレームの合計を合算する
+      point += frame.sum
+    end
+  end
+  puts point
 end
 
 bowling
