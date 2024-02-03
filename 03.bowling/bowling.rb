ffff336f8276b21b6126fb65019a2a18fac7fba8
yokomaru <cabochetoganlan@gmail.com>
2024-02-03 18:08:25 +0900

    fix: rubocopの指摘修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -7,9 +7,9 @@ SPARE_SUM_SCORE = 10
 LAST_FRAME = 9
 
 def bowling
-  scores = ARGV[0].split(",")
-  shots = scores.each_with_object([]).with_index(1) do |(score, shot), index|
-    if score == "X"
+  scores = ARGV[0].split(',')
+  shots = scores.each_with_object([]) do |score, shot|
+    if score == 'X'
       shot << STRIKE_FIRST_SHOT_SCORE
       shot << STRIKE_SECOND_SHOT_SCORE
     else
@@ -26,24 +26,25 @@ def bowling
   frames.each_with_index do |frame, index|
     # 10フレーム目以降は計算しない
     break if index > LAST_FRAME
+
     # ストライクだった場合
-    if frame[0] == STRIKE_FIRST_SHOT_SCORE
-      # 次もストライクだった場合
-      if frames[index + 1][0] == STRIKE_FIRST_SHOT_SCORE
-        # 次とその次のフレームの１投目を合計した値を合算する
-        point += frame[0] + frames[index + 1][0] + frames[index + 2][0]
-      # 次がストライクじゃなかった場合は次のフレームの１投目と２投目を合算する
-      else
-        point += frame[0] + frames[index + 1][0] + frames[index + 1][1]
-      end
-    # スペアだった場合
-    elsif frame.sum == SPARE_SUM_SCORE
-      # 次のフレームの１投目を合算する
-      point += frame.sum + frames[index + 1][0]
-    else
-      # 現在のフレームの合計を合算する
-      point += frame.sum
-    end
+    point += if frame[0] == STRIKE_FIRST_SHOT_SCORE
+                # 次もストライクだった場合
+                if frames[index + 1][0] == STRIKE_FIRST_SHOT_SCORE
+                  # 次とその次のフレームの１投目を合計した値を合算する
+                  frame[0] + frames[index + 1][0] + frames[index + 2][0]
+                  # 次がストライクじゃなかった場合は次のフレームの１投目と２投目を合算する
+                else
+                  frame[0] + frames[index + 1][0] + frames[index + 1][1]
+                end
+              # スペアだった場合
+            elsif frame.sum == SPARE_SUM_SCORE
+              # 次のフレームの１投目を合算する
+              frame.sum + frames[index + 1][0]
+            else
+              # 現在のフレームの合計を合算する
+              frame.sum
+            end
   end
   puts point
 end
