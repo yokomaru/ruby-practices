07446bed12254e72243d55208955092e40c9c4a2
yokomaru <cabochetoganlan@gmail.com>
2024-02-07 21:28:24 +0900

    fix: 処理が短くなりメソッド化する必要がなくなった箇所をgenerate_calender処理に移動 + ブロック処理をdo ~ end から { }に修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -6,56 +6,20 @@ require 'date'
 
 def generate_calender
   params = ARGV.getopts('m:', 'y:')
-  month = generate_month(params['m'])
-  year = generate_year(params['y'])
+
+  month = params['m'].to_i.between?(1, 12) ? params['m'].to_i : Date.today.month
+  year = params['y'].to_i.between?(1970, 2100) ? params['y'].to_i : Date.today.year
 
   first_date = Date.new(year, month, 1)
   last_date = Date.new(year, month, -1)
 
-  calender_dates = generate_calender_dates(first_date, last_date)
-
-  carendar_display_data = generate_carendar_display_data(first_date, calender_dates)
+  calender_dates = (first_date..last_date).map { |date| date == Date.today ? "\e[30m\e[47m#{date.day.to_s.rjust(2)}\e[0m" : date.day.to_s.rjust(2) }
+  first_date.wday.times { calender_dates.unshift('　') }
 
   puts "#{first_date.month}月 #{first_date.year}".center(20)
   puts '日 月 火 水 木 金 土'
-
-  carendar_display_data.each do |carendar_display_data|
-    puts carendar_display_data.join(' ')
-  end
-
+  calender_dates.each_slice(7) { |calender_date| puts calender_date.join(' ') }
   puts "\n"
-
-end
-
-def generate_month(param_month)
-  param_month.to_i.between?(1, 12) ? param_month.to_i : Date.today.month
-end
-
-def generate_year(param_year)
-  param_year.to_i.between?(1970, 2100) ? param_year.to_i : Date.today.year
-end
-
-def generate_calender_dates(first_date, last_date)
-  (first_date..last_date).map do |date|
-    if date == Date.today
-      "\e[30m\e[47m#{date.day.to_s.rjust(2)}\e[0m"
-    else
-      date.day.to_s.rjust(2)
-    end
-  end
-end
-
-def generate_carendar_display_data(first_date, calender_dates)
-  first_date.wday.times do
-    calender_dates.unshift('　')
-  end
-
-  carendar_display_data = []
-  calender_dates.each_slice(7) do |calender_date|
-    carendar_display_data << calender_date
-  end
-
-  carendar_display_data
 end
 
 generate_calender
