1a11ad381ba5858c892b10c9c334583179094a0f
yokomaru <cabochetoganlan@gmail.com>
2024-03-08 13:20:59 +0900

    fix: マジックナンバーを定数に変更＋不要なメソッドの整理や重複の削除

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,93 +4,75 @@
 require 'optparse'
 require 'debug'
 
-TERMINAL_WIDTH = IO.console_size[1]
 MAX_COLUMN = 3
 MIN_COLUMN = 1
 BUFFER_WIDTH = 1
+NORMAL_BYTE_SIZE = 1
+MARTI_BYTE_SIZE = 2
 
 def main
   args = ARGV.sort
   arg_directories = []
   arg_files = []
   arg_directories << Dir.open('.') if args.empty?
+
   args.each do |arg|
     next arg_directories << Dir.open(arg) if File.directory?(arg)
     next arg_files << arg if File.file?(arg)
 
     puts "ls: #{arg}: No such file or directory"
   end
-  return if check_directories_and_files_size(arg_directories, arg_files)
 
   display_files(arg_files, arg_files.count)
   puts if exist_directories_and_files?(arg_directories, arg_files)
-  display_directories_files(arg_directories)
+  display_directories(arg_directories, args.size)
 end
 
-def display_files(arg_files, files_count)
-  return if arg_files.empty?
+def exist_directories_and_files?(directories, files)
+  !directories.empty? && !files.empty?
+end
 
-  sorted_files = sort_directory_files(arg_files)
-  max_file_name = fetch_max_file_name(sorted_files)
-  display_width = calculate_display_width(max_file_name)
+def display_files(files, files_count)
+  return if files.empty?
+
+  generated_files = generate_display_files(files, files_count)
+  transpose_display_files(generated_files)
+end
+
+def display_directories(directories, args_size)
+  return if directories.empty?
+
+  directories_count = directories.size
+  directories.each.with_index(1) do |directory, i|
+    puts "#{directory.path}:" if args_size > 1
+
+    directory_files = []
+    directory.each_child do |file|
+      directory_files << file unless /^\./.match?(file)
+    end
+    files_count_in_directory = directory_files.count
+
+    if files_count_in_directory >= 1
+      generated_files = generate_display_files(directory_files, files_count_in_directory)
+      transpose_display_files(generated_files)
+    end
+
+    puts if directories_count >= 1 && i < directories_count
+  end
+end
+
+def generate_display_files(files, files_count)
+  sorted_files = files.map { |file| file }.sort
+  longest_filename_size = sorted_files.map(&:bytesize).max
+  column_size = calculate_column_size(longest_filename_size, files_count)
+  slice_column_size_display_files(sorted_files, column_size, longest_filename_size)
+end
+
+def calculate_column_size(longest_filename_size, files_count)
+  # IO.console_size[1] = 実行するターミナルの幅を取得
+  display_width = IO.console_size[1] / (longest_filename_size + BUFFER_WIDTH)
   slice_number = fetch_slice_number(display_width)
-  column_size = calculate_column_size(files_count, slice_number)
-  display_files = fetch_display_files(sorted_files, column_size, max_file_name)
-  display_files.transpose.each { |files| puts files.join(' ') }
-end
-
-def display_directories_files(arg_directories)
-  return if arg_directories.empty?
-
-  arg_directories.each.with_index(1) do |directory, i|
-    sorted_files = generate_directory_files(directory)
-    max_file_name = fetch_max_file_name(sorted_files)
-    files_count = sorted_files.count
-    display_width = calculate_display_width(max_file_name)
-    slice_number = fetch_slice_number(display_width)
-    column_size = calculate_column_size(files_count, slice_number)
-    display_directory_files = fetch_display_files(sorted_files, column_size, max_file_name)
-    puts "#{directory.path}:" if arg_directories.size > 1
-    display_directory_files.transpose.each { |files| puts files.join(' ') }
-    puts if arg_directories.size > 1 && i < arg_directories.size
-  end
-end
-
-def check_directories_and_files_size(arg_directories, arg_files)
-  arg_directories.count.zero? && arg_files.count.zero?
-end
-
-def exist_directories_and_files?(arg_directories, arg_files)
-  !arg_files.empty? && !arg_directories.empty?
-end
-
-def generate_directory_files(directory)
-  directory_files = fetch_directory_files(directory)
-  sort_directory_files(directory_files)
-end
-
-def fetch_directory_files(directory)
-  directory_file_names_and_file_sizes = []
-  directory.each_child do |file|
-    directory_file_names_and_file_sizes << file unless /^\./.match?(file)
-  end
-  directory_file_names_and_file_sizes
-end
-
-def fetch_max_file_name(directory_files)
-  directory_files.map(&:bytesize).max
-end
-
-def sort_directory_files(files)
-  files.map { |file| file }.sort
-end
-
-def calculate_display_width(name)
-  TERMINAL_WIDTH / (name + BUFFER_WIDTH)
-end
-
-def calculate_column_size(size, number)
-  (size.to_f / number).ceil
+  (files_count.to_f / slice_number).ceil
 end
 
 def fetch_slice_number(width)
@@ -103,18 +85,22 @@ def fetch_slice_number(width)
   end
 end
 
-def fetch_display_files(sorted_directory_files, column_size, max_file_name)
-  sorted_directory_files.each_slice(column_size).map do |files|
-    sliced_files = files.map { |file| file.ljust(calculate_align_left_size(file, max_file_name)) }
+def slice_column_size_display_files(sorted_files, column_size, longest_filename_size)
+  sorted_files.each_slice(column_size).map do |files|
+    sliced_files = files.map { |file| file.ljust(calculate_align_left_size(file, longest_filename_size)) }
     (column_size - sliced_files.size).times { sliced_files << '' } if sliced_files.size < column_size
     sliced_files
   end
 end
 
-def calculate_align_left_size(string, size)
-  output_width = string.each_char.map { |c| c.bytesize == 1 ? 1 : 2 }.reduce(0, &:+)
-  padding_size = [0, size - output_width].max
-  padding_size + string.size
+def calculate_align_left_size(file_name, longest_filename_size)
+  adjusted_byte_number = file_name.each_char.map { |c| c.bytesize == NORMAL_BYTE_SIZE ? NORMAL_BYTE_SIZE : MARTI_BYTE_SIZE }.reduce(0, &:+)
+  padding_size = [0, longest_filename_size - adjusted_byte_number].max
+  padding_size + file_name.size
+end
+
+def transpose_display_files(display_files)
+  display_files.transpose.each { |files| puts files.join(' ') }
 end
 
 main
