b29237ec2224771dacbe07d754b7c9362dd951d2
yokomaru <cabochetoganlan@gmail.com>
2024-05-31 14:21:26 +0900

    add: my-wc

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -1,18 +1,64 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'optparse'
+
+DISPLAY_BUFFER_WIDTH = 8
+
 # mainの作成
 def main
-  # 固定でファイル指定して読み込み
-  # 読み込んだファイルを配列として受け取り
-
-  # wオプション のデータ表示
-  # lオプション のデータ表示
-  # cオプション のデータ表示
-  # 全てのオプション のデータ表示
-
-  # 引数の受け取り
-  # 複数ファイル
-    # totalの表示
-  # コマンドの受け取り
+  args = ARGV
+  options = parse_options(args)
+  strings_for_count = generate_count_strings(args)
+  counts = []
+  strings_for_count.map do |string|
+    begin
+      line = args.empty? ? string : File.read(string)
+      count = []
+      count << line.count("\n") if options[:l]
+      count << line.split.size if options[:w]
+      count << line.bytesize if options[:c]
+      counts << count
+      puts_count_string(count, args, string)
+    rescue SystemCallError => e
+      puts_error_message(string)
+      counts << [0, 0, 0]
+      next
+    end
+  end
+  count_total(counts, args)
 end
+
+def parse_options(args)
+  options = args.getopts('wcl', symbolize_names: true)
+  options = {w: true, c: true, l: true} if options.values.none?
+  options
+end
+
+def generate_count_strings(args)
+  args.empty? ? [$stdin.readlines.join] : args
+end
+
+def puts_error_message(string)
+  if File.directory?(string)
+    puts "wc: #{string}: read: Is a directory"
+  else
+    puts "wc: #{string}: open: No such file or directory"
+  end
+end
+
+def puts_count_string(count, args, string)
+  file_name = string if !args.empty?
+  puts "#{display_rjust(count)} #{file_name}"
+end
+
+def count_total(counts, args)
+  totals = counts.transpose.map{|count| count.sum}
+  puts "#{display_rjust(totals)} total" if args.size > 1
+end
+
+def display_rjust(array)
+  array.map{|a| a.to_s.rjust(DISPLAY_BUFFER_WIDTH)}.join
+end
+
+main if $PROGRAM_NAME == __FILE__
