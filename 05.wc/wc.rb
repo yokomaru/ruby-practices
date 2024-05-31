ad7800699908b05801fed34b33684078ed6cd71e
yokomaru <cabochetoganlan@gmail.com>
2024-05-31 14:38:40 +0900

    fix: rubocop and method name
    rubocop指摘箇所修正
    メソッド名が少しわかりづらかったのでgenerate_count_strings→generate_strings_for_countに変更
    変数名lineがわかりづらかったので読み込んでいる行としてrowに変更

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -9,33 +9,32 @@ DISPLAY_BUFFER_WIDTH = 8
 def main
   args = ARGV
   options = parse_options(args)
-  strings_for_count = generate_count_strings(args)
+  strings = generate_strings_for_count(args)
   counts = []
-  strings_for_count.map do |string|
-    begin
-      line = args.empty? ? string : File.read(string)
-      count = []
-      count << line.count("\n") if options[:l]
-      count << line.split.size if options[:w]
-      count << line.bytesize if options[:c]
-      counts << count
-      puts_count_string(count, args, string)
-    rescue SystemCallError => e
-      puts_error_message(string)
-      counts << [0, 0, 0]
-      next
-    end
+  strings.map do |string|
+    row = args.empty? ? string : File.read(string)
+    count = []
+    count << row.count("\n") if options[:l]
+    count << row.split.size if options[:w]
+    count << row.bytesize if options[:c]
+    counts << count
+    puts_count_string(count, args, string)
+  rescue SystemCallError
+    puts_error_message(string)
+    counts << [0, 0, 0]
+    next
   end
   count_total(counts, args)
 end
 
 def parse_options(args)
   options = args.getopts('wcl', symbolize_names: true)
-  options = {w: true, c: true, l: true} if options.values.none?
+  options = { w: true, c: true, l: true } if options.values.none?
   options
 end
 
-def generate_count_strings(args)
+def generate_strings_for_count(args)
+  # 引数がある場合は標準入力より引数の入力を優先させる
   args.empty? ? [$stdin.readlines.join] : args
 end
 
@@ -53,12 +52,12 @@ def puts_count_string(count, args, string)
 end
 
 def count_total(counts, args)
-  totals = counts.transpose.map{|count| count.sum}
+  totals = counts.transpose.map(&:sum)
   puts "#{display_rjust(totals)} total" if args.size > 1
 end
 
 def display_rjust(array)
-  array.map{|a| a.to_s.rjust(DISPLAY_BUFFER_WIDTH)}.join
+  array.map { |a| a.to_s.rjust(DISPLAY_BUFFER_WIDTH) }.join
 end
 
 main if $PROGRAM_NAME == __FILE__
