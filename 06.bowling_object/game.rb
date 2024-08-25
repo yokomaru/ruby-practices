5827ffc3692fcc24925a4f882f9eb77d01093ce9
yokomaru <cabochetoganlan@gmail.com>
2024-08-25 14:59:58 +0900

    fix: Game
    - shotsとscoreという変数名が区別つけづらかったので、score_textとscoreに変更
    - スコアを配列にする処理を別メソッドに分ける(parse_score_text)
    - スコアに関する定数をShotに移動する
    - 実行処理を追加
    - 使わなかったbowling.rbを削除
    - rubocopでの指摘事項frozen_string_literal: trueを追加

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -1,47 +1,34 @@
+# frozen_string_literal: true
+
 require_relative 'frame'
-require_relative 'bowling'
 
 class Game
-  def initialize(shots)
-    @shots = shots
+  GAME_TIMES = 10
+
+  def initialize(score_text)
+    @score_text = score_text
   end
-  # FRAMEに移動する
-  FRAME_TIMES = 10
-  STRIKE_SCORE = 10
-  SPARE_SCORE = 10
 
-  # 引数で渡すようにする
-  # FRAMEに移動する
-  def calc_scores
-    scores = @shots.split(',').map { |score| score == 'X' ? STRIKE_SCORE : score.to_i }
+  def score
+    # メソッド分ける
+    scores = parse_score_text(@score_text)
     total_point = 0
-    frame_head = 0
-    FRAME_TIMES.times do
-      # puts scores[frame_head]
-      # puts scores[frame_head + 1]
-      # puts scores[frame_head] + scores[frame_head + 1]
-      # puts scores[frame_head] + scores[frame_head + 1] == SPARE_SCORE
-      # FRAMEに移動する
-      total_point += if scores[frame_head] == STRIKE_SCORE || scores[frame_head] + scores[frame_head + 1] == SPARE_SCORE
-                       frame = Frame.new(scores[frame_head], scores[frame_head + 1], scores[frame_head + 2])
-                       #  puts 'スペアかストライク'
-                       #  puts scores[frame_head + 2]
-                       #  puts frame.score
-                       frame.score
-                     else
-                       frame = Frame.new(scores[frame_head], scores[frame_head + 1])
-                       frame.score
-                     end
-
-      frame_head += scores[frame_head] == STRIKE_SCORE ? 1 : 2
+    GAME_TIMES.times do
+      frame = Frame.new(scores[0], scores[1], scores[2])
+      total_point += frame.score
+      scores.shift(scores[0] == Shot::STRIKE_SCORE ? 1 : 2)
     end
     total_point
   end
+
+  private
+
+  def parse_score_text(score_text)
+    score_text.split(',').map { |score| score == 'X' ? Shot::STRIKE_SCORE : score.to_i }
+  end
 end
 
-# marks = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X'
-# game = Game.new(marks)
-# assert_equal 164, game.calc_scores
-
-# # テストを追加
+if __FILE__ == $PROGRAM_NAME
+  game = Game.new(ARGV[0])
+  puts game.score
 end
