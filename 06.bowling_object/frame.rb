55b57430f275d26c4433c0203acd483e45d0dccb
yokomaru <cabochetoganlan@gmail.com>
2024-09-08 16:40:09 +0900

    fix: move full score constant Shot to Frame
    - ストライクやスペアは単体のShotでは判定できずFrameで決まるため、Shotにある定数をFrameに移す
    - ストライク・スペアの合計得点である10を入れる定数として命名をFULL_SCOREに変更
        - Shot内のscore内で単体のshotとFrameのFULL_SCOREで比較している箇所を単純に満点の10点と比較するように修正
    - shotの点数を定数と比較して分岐していた箇所が長くなりすぎたため、strike?とspare?メソッドを作成

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -3,6 +3,8 @@
 require_relative 'shot'
 
 class Frame
+  FULL_SCORE = 10
+
   def initialize(first_mark, second_mark, third_mark)
     @first_shot = Shot.new(first_mark)
     @second_shot = Shot.new(second_mark)
@@ -10,10 +12,18 @@ class Frame
   end
 
   def score
-    if @first_shot.score == Shot::STRIKE_SCORE || @first_shot.score + @second_shot.score == Shot::SPARE_SCORE
+    if strike? || spare?
       [@first_shot, @second_shot, @third_shot].map(&:score).sum
     else
       [@first_shot, @second_shot].map(&:score).sum
     end
+  end
+
+  def strike?
+    @first_shot.score == FULL_SCORE
+  end
+
+  def spare?
+    @first_shot.score + @second_shot.score == FULL_SCORE
   end
 end
--- 06.bowling_object/shot.rb
+++ 06.bowling_object/shot.rb
@@ -1,14 +1,11 @@
 # frozen_string_literal: true
 
 class Shot
-  STRIKE_SCORE = 10
-  SPARE_SCORE = 10
-
   def initialize(mark)
     @mark = mark
   end
 
   def score
-    @mark == 'X' ? STRIKE_SCORE : @mark.to_i
+    @mark == 'X' ? 10 : @mark.to_i
   end
 end
