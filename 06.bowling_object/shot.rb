c1aa2f14777496e5f9938aab97c62d6f4b265984
yokomaru <cabochetoganlan@gmail.com>
2024-09-11 23:52:10 +0900

    fix: Shot have point as a variable
    - FrameからShotを呼ぶ際、毎回scoreメソッドで文字列を数値にパースしていたので、@scoreという変数にデータとして数値の情報を持っておくように修正
    - 文字列はmarkとしてそのまま持っておく

--- 06.bowling_object/shot.rb
+++ 06.bowling_object/shot.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 class Shot
+  attr_reader :score
+
   def initialize(mark)
     @mark = mark
+    @score = parse_score
   end
 
-  def score
+  private
+
+  def parse_score
     @mark == 'X' ? 10 : @mark.to_i
   end
 end
