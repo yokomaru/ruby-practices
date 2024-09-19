534db113c12772d6c006e9be07569355e9e020c2
yokomaru <cabochetoganlan@gmail.com>
2024-09-19 19:40:53 +0900

    add: instance variable short_format_data
    - 引数としてshort_format_dataを持っていなかったので持たせ、renderでjoinして表示するように修正

--- 07.ls_object/lib/format/short_format.rb
+++ 07.ls_object/lib/format/short_format.rb
@@ -6,16 +6,28 @@ class ShortFormat < Format
   def initialize(files, width)
     super(files)
     @width = width
-    @max_file_name = @files.map { |f| File.basename(f.name).size }.max
+    @max_file_name = @files.map { |file| File.basename(file.name).size }.max
     @col_count = calculate_col_count
     @row_count = calculate_row_count
+    @short_format_data = build_short_format_data
   end
 
   def render
-    safe_transpose.map { |row_files| render_short_format_row(row_files) }.join("\n")
+    @short_format_data.join("\n")
   end
 
   private
+
+  def build_short_format_data
+    safe_transpose.map { |row_files| build_short_format_row(row_files) }
+  end
+
+  def build_short_format_row(row_files)
+    row_files.map do |files|
+      basename = files ? File.basename(files.name) : ''
+      basename.ljust(@max_file_name + 1)
+    end.join.rstrip
+  end
 
   def calculate_col_count
     @width / (@max_file_name + 1)
@@ -28,12 +40,5 @@ class ShortFormat < Format
   def safe_transpose
     nested_file_names = @files.each_slice(@row_count).to_a
     nested_file_names[0].zip(*nested_file_names[1..])
-  end
-
-  def render_short_format_row(row_files)
-    row_files.map do |files|
-      basename = files ? File.basename(files.name) : ''
-      basename.ljust(@max_file_name + 1)
-    end.join.rstrip
   end
 end
