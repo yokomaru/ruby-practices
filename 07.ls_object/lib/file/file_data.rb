a99e31fcff81f8dadef5d129d3835894dc3dad5a
yokomaru <cabochetoganlan@gmail.com>
2024-09-30 11:13:03 +0900

    fix: integrate FileData and FileStatus
    - FileDataとFileStatusは1対1なのでクラスを分ける必要がないため、FileStatusを削除しFileDataにステータス関連の情報を生成する処理を統合する

--- 07.ls_object/lib/file/file_data.rb
+++ 07.ls_object/lib/file/file_data.rb
@@ -1,30 +1,49 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'etc'
 require 'pathname'
-require_relative 'file_status'
 
 class FileData
   attr_reader :name, :file_status
 
+  MODE_TABLE = {
+    '0' => '---',
+    '1' => '--x',
+    '2' => '-w-',
+    '3' => '-wx',
+    '4' => 'r--',
+    '5' => 'r-x',
+    '6' => 'rw-',
+    '7' => 'rwx'
+  }.freeze
+
   def initialize(name, path)
     @name = name
     @full_path = Pathname(File.absolute_path(@name, path))
-    @file_status = build_file_status(FileStatus.new(@full_path))
+    @file_status = build_file_status(File::Stat.new(@full_path))
   end
 
   private
 
-  def build_file_status(file_status)
+  def build_file_status(status)
     {
-      type_and_mode: "#{file_status.file_type}#{file_status.file_mode}",
-      hardlink_nums: file_status.hardlink_nums.to_s,
-      owner_name: file_status.owner_name,
-      group_name: file_status.group_name,
-      bytesize: file_status.bytesize.to_s,
-      latest_modify_datetime: file_status.latest_modify_datetime,
+      type_and_mode: "#{format_file_type}#{format_file_mode}",
+      hardlink_nums: status.nlink.to_s,
+      owner_name: Etc.getpwuid(status.uid).name,
+      group_name: Etc.getgrgid(status.gid).name,
+      bytesize: status.size.to_s,
+      latest_modify_datetime: status.mtime.strftime('%_m %e %H:%M'),
       filename: @name,
-      blocks: file_status.blocks
+      blocks: status.blocks
     }
+  end
+
+  def format_file_type
+    @full_path.directory? ? 'd' : '-'
+  end
+
+  def format_file_mode
+    @full_path.stat.mode.to_s(8)[-3..].gsub(/./, MODE_TABLE)
   end
 end
